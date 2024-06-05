a96744fd774f27b3f4c230e1883cbacf6a5e5312
Ruslan Mogilevskiy <ruslan.mogilevskiy@betssongroup.com>
2024-06-05 22:59:42 +0300

    Add an example gif to readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Resharper.QuickActions
-Quick context actions for Resharper that are accessible via 'Quick actions' bulb:
+
+![Example](./docs/images/example.gif)
+
+Quick context actions for Resharper that are accessible via the 'Quick actions' bulb:
  * Clone type - clones the focused class. The cursor must be either on the class Name or its body (empty space).
  * Clone method - clones the focused method. The cursor must be either on the method Name or its body (empty space).
- * Clone property - clones the focused property.
+ * Clone property - clones the focused property. Place cursor on any part of the target property definition.
--- src/Resharper.QuickActions/Constants.cs
+++ src/Resharper.QuickActions/Constants.cs
@@ -1,10 +1,99 @@
-﻿namespace Rumo.Resharper.QuickActions
+﻿using System.Collections.Generic;
+
+namespace Rumo.Resharper.QuickActions
 {
     static class Constants
     {
+        public string Foo { get; set; }
+
         public class Languages
         {
             public const string CSharp = "C#";
         }
     }
+}
+
+
+namespace Test
+{
+    public static class Foo
+    {
+        static Dictionary<string, object> Locks { get; } = new Dictionary<string, object>();
+        static Dictionary<string, object> Locks2 { get; } = new Dictionary<string, object>();
+
+        public static object GetLock(string name)
+        {
+            return Locks[name];
+        }
+
+        public static object GetLock2(string name)
+        {
+            return Locks2[name];
+        }
+    }
+
+    public static class Foo2
+    {
+        static Dictionary<string, object> Locks { get; } = new Dictionary<string, object>();
+        static Dictionary<string, object> Locks2 { get; } = new Dictionary<string, object>();
+
+        public static object GetLock(string name)
+        {
+            return Locks[name];
+        }
+
+        public static object GetLock2(string name)
+        {
+            return Locks2[name];
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
